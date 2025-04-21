cdc1b85872374617ce7a2d16b2d0530a93b4b137
Alistair Abao <abaolester@gmail.com>
2025-04-21 18:15:00 +0200

    register user functionality with password hashing using passportJs

--- server/dist/ExpressError/ExpressError.js
+++ server/dist/ExpressError/ExpressError.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ExpressError = void 0;
 class ExpressError extends Error {
-    constructor({ message, status }) {
+    constructor(message, status) {
         super();
         this.message = message;
         this.status = status;
