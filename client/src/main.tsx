c3013741e7edb3e56e836daa8052dcc7bccebd5f
Alistair Abao <abaolester@gmail.com>
2025-05-06 15:50:15 +0200

    created a component that wraps around protected components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { ToastContainer, Zoom } from "react-toastify";
 /** @contextClass object to apply colors to toast alerts */
 const contextClass = {
   success: "bg-custom-green",
-  error: "bg-red-600",
+  error: "bg-red-300 text-white ",
   info: "bg-gray-600",
   warning: "bg-orange-400",
   default: "bg-indigo-600",
